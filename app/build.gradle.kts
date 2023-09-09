9bb249449af76653720c8cdeb1a12c3c460968d5
lihang <lihangleo2@163.com>
2023-09-09 14:07:36 +0800

    解决发布bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 29
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
