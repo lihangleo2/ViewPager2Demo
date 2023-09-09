59c40850eb0e75626c488e43a776e5b636117d03
lihang <lihangleo2@163.com>
2023-09-09 18:10:46 +0800

    jitpack bug fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,9 +38,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.5.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(project(mapOf("path" to ":smartVpageAdapterlibrary")))
     testImplementation("junit:junit:4.13.2")
